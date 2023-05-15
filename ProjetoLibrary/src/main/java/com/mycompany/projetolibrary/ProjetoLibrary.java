ed11b65d8772fe125dc89341c65f32bb3a3d4a5e
khuunsh68 <goncalofarias10@ua.pt>
2023-05-15 11:19:06 +0100

    Código melhorado

--- ProjetoLibrary/src/main/java/com/mycompany/projetolibrary/ProjetoLibrary.java
+++ ProjetoLibrary/src/main/java/com/mycompany/projetolibrary/ProjetoLibrary.java
@@ -9,14 +9,61 @@ import java.util.*;
  *
  * @author goncalo farias
  */
-class BuffFIFO<Object> {
+class Buffer<Object> {
 
-    private LinkedList<Object> buffer = new LinkedList<>();
+    protected LinkedList<Object> buffer = new LinkedList<>();
 
+    // Método que adiciona elemento ao buffer
+    public void addElemento(Object elemento) {
+        // Método a implementar nas classes FIFO e LIFO
+    }
+
+    // Método que remove elemento do buffer
+    public Object removeElemento() {
+        // Método a implementar nas classes FIFO e LIFO
+        return null;
+    }
+
+    // Método que mostra elements do buffer
+    public void mostrarElementos() {
+        System.out.println("Buffer atualizado: ");
+        for (int i = 0; i < buffer.size(); i++) {
+            System.out.println("Posicao " + i + ": " + buffer.get(i));
+        }
+    }
+
+    // Método que reseta o buffer
+    public void limpaBuffer() {
+        buffer.clear();
+        System.out.println("Buffer limpo!");
+    }
+
+    // Método que vê o tamanho do buffer
+    public int tamanho() {
+        return buffer.size();
+    }
+
+    public void substituirElemento(int posicao, Object novoElemento) {
+
+        if (posicao >= 0 && posicao < buffer.size()) {
+            buffer.set(posicao, novoElemento);
+            System.out.println("Elemento substituido na posicao " + posicao + ": " + novoElemento);
+        } else {
+            System.out.println("Posicao invalida!");
+        }
+    }
+}
+
+class BuffFIFO<Object> extends Buffer<Object> {
+
+    // Método que adiciona elementos ao buffer
+    @Override
     public void addElemento(Object elemento) {
         buffer.addLast(elemento);
     }
 
+    // Método que remove elemento do buffer
+    @Override
     public Object removeElemento() {
         if (!buffer.isEmpty()) {
             return buffer.removeFirst();
@@ -25,78 +72,46 @@ class BuffFIFO<Object> {
         }
     }
 
-    public void mostrarElementos() {
-        System.out.println("Buffer atualizado: ");
-        for (int i = 0; i < buffer.size(); i++) {
-            System.out.println("Posicao " + i + ": " + buffer.get(i));
-        }
-    }
-
-    public String vazio() {
-        if (buffer.isEmpty()) {
-            return "Buffer FIFO esta vazio!";
-        } else {
-            return "Buffer FIFO nao esta vazio!";
-        }
-    }
-
-    public int tamanho() {
-        return buffer.size();
-    }
 }
 
-class BuffLIFO<Object> {
+class BuffLIFO<Object> extends Buffer<Object> {
 
-    private LinkedList<Object> buffer = new LinkedList<>();
-
+    // Método que adiciona elementos ao buffer
+    @Override
     public void addElemento(Object elemento) {
         buffer.addFirst(elemento);
     }
 
+    // Método que remove elemento do buffer
+    @Override
     public Object removeElemento() {
         if (!buffer.isEmpty()) {
             return buffer.removeFirst();
         } else {
             return (Object) "Nao existem mais elementos a remover!";
         }
-    }
-
-    public void mostrarElementos() {
-        System.out.println("Buffer atualizado: ");
-        for (int i = 0; i < buffer.size(); i++) {
-            System.out.println("Posicao " + i + ": " + buffer.get(i));
-        }
-    }
-
-    public String vazio() {
-        if (buffer.isEmpty()) {
-            return "Buffer FIFO esta vazio!";
-        } else {
-            return "Buffer FIFO nao esta vazio!";
-        }
-    }
-
-    public int tamanho() {
-        return buffer.size();
     }
 }
 
 class Menus {
 
+    // Método que contém o menu da aplicação
     public int menuPrincipal(Scanner sc) {
         // Menu principal da Aplicação
         System.out.println("\n***** Menu *****");
         System.out.println("1. Adicionar elemento");
         System.out.println("2. Remover elemento");
         System.out.println("3. Mostrar elementos");
-        System.out.println("4. Verificar se está vazio");
-        System.out.println("5. Verificar tamanho");
-        System.out.println("6. Sair");
+        System.out.println("4. Substituir posicao");
+        System.out.println("5. Limpa Buffer");
+        System.out.println("6. Verificar tamanho");
+        System.out.println("7. Sair");
         System.out.println("Opcao: ");
 
         return sc.nextInt();
     }
 
+    // Método que trabalha as opções do menu
     public void mostraAplicacao() {
         // Scan para a escolha da opção
         Scanner sc = new Scanner(System.in);
@@ -142,22 +157,32 @@ class Menus {
                             break;
 
                         case 4:
-                            //Verifica se está vazio
-                            System.out.println(buffFifo.vazio());
+                            //Substitui elemento
+                            System.out.println("Posicao do elemento a substituir: ");
+                            int posicaoSubstituir = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Novo elemento: ");
+                            Object novoElementoBuff = sc.nextLine();
+                            buffFifo.substituirElemento(posicaoSubstituir, novoElementoBuff);
                             break;
 
                         case 5:
+                            //Limpa Buffer
+                            buffFifo.limpaBuffer();
+                            break;
+
+                        case 6:
                             //Verifica tamanho
                             System.out.println("Tamanho do buffer: " + buffFifo.tamanho());
                             break;
 
-                        case 6:
+                        case 7:
                             break;
 
                         default:
                             System.out.println("Opcao invalida!Tente novamente!");
                     }
-                } while (opcaoMenu != 6);
+                } while (opcaoMenu != 7);
                 break;
             case 2:
                 // Criação de um buffer LIFO
@@ -190,22 +215,32 @@ class Menus {
                             break;
 
                         case 4:
-                            //Verifica se está vazio
-                            System.out.println(buffLifo.vazio());
+                            //Substitui elemento
+                            System.out.println("Posicao do elemento a substituir: ");
+                            int posicaoSubstituir = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Novo elemento: ");
+                            Object novoElementoBuff = sc.nextLine();
+                            buffLifo.substituirElemento(posicaoSubstituir, novoElementoBuff);
                             break;
 
                         case 5:
+                            //Limpa Buffer
+                            buffLifo.limpaBuffer();
+                            break;
+
+                        case 6:
                             //Verifica tamanho
                             System.out.println("Tamanho do buffer: " + buffLifo.tamanho());
                             break;
 
-                        case 6:
+                        case 7:
                             break;
 
                         default:
                             System.out.println("Opcao invalida!Tente novamente!");
                     }
-                } while (opcaoMenu2 != 6);
+                } while (opcaoMenu2 != 7);
                 break;
             case 3:
                 break;
